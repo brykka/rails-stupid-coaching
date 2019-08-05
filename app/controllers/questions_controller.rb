6fc3a34409a7a2c511312d725fae3756045686d0
Bryenne Kay <bryenne.k@gmail.com>
2019-08-05 14:47:12 +0900

    added questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,16 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    # http://localhost:3000/answer?question=hi
+    # p name = 'params_question'
+    @questions = params['question']
+    @answer = 'random answer'
+    if @questions == 'I am going to work'
+      @answer = 'Great!'
+    elsif @questions.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
